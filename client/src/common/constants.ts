96f24c0a940a33f77ffe4ae1f0d3d7a88672ecc3
lsw1164 <qkqkfo@gmail.com>
2022-02-13 20:08:16 +0900

    :wrench: Set Config

---
+++ client/src/common/constants.ts
@@ -0,0 +1,3 @@
+export const RoutePath = {
+  ProductList: "/",
+} as const;
---
+++ client/src/pages/ProductListPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductListPage: React.FC = () => {
+  return <span>Product List Page</span>;
+};
+
+export default ProductListPage;
